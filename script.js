d3cecfa6ac4cadcd4a58864604e4b8317c209622
AQILAHRAJAB <AQILAH@MAJULAH.SG>
2019-07-08 17:38:17 +0800

    Full function including clue cards and suspect pictures are up

--- script.js
+++ script.js
@@ -7,17 +7,68 @@ var suspectDeduced = [];
 console.log(suspectDeduced);
 console.log(suspectDeduced.length);
 
+
 //Starting prompt to insert player's name
 var detectiveName = prompt("Reveal your identity!")
 
 //Change image upon click to eliminate the suspects
 function changeImage(){
     var image = document.getElementById("pic_s1");
-    if (image.src.match("images/Testing-image.png")) {
+    if (image.src.match("images/Suspect1.png")) {
         image.src = "images/Eliminated-Suspect.png";
     }
     else {
-        image.src = "images/Testing-image.png";
+        image.src = "images/Suspect1.png";
+    }
+}
+
+function changeImage2(){
+    var image = document.getElementById("pic_s2");
+    if (image.src.match("images/Suspect2.png")) {
+        image.src = "images/Eliminated-Suspect.png";
+    }
+    else {
+        image.src = "images/Suspect2.png";
+    }
+}
+
+function changeImage3(){
+    var image = document.getElementById("pic_s3");
+    if (image.src.match("images/Suspect3.png")) {
+        image.src = "images/Eliminated-Suspect.png";
+    }
+    else {
+        image.src = "images/Suspect3.png";
+    }
+}
+
+function changeImage4(){
+    var image = document.getElementById("pic_s4");
+    if (image.src.match("images/Suspect4.png")) {
+        image.src = "images/Eliminated-Suspect.png";
+    }
+    else {
+        image.src = "images/Suspect4.png";
+    }
+}
+
+function changeImage5(){
+    var image = document.getElementById("pic_s5");
+    if (image.src.match("images/Suspect5.png")) {
+        image.src = "images/Eliminated-Suspect.png";
+    }
+    else {
+        image.src = "images/Suspect5.png";
+    }
+}
+
+function changeImage6(){
+    var image = document.getElementById("pic_s6");
+    if (image.src.match("images/Suspect6.png")) {
+        image.src = "images/Eliminated-Suspect.png";
+    }
+    else {
+        image.src = "images/Suspect6.png";
     }
 }
 
